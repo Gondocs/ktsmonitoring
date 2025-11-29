bc8a2313e872a5538cda0fd93669f2f2882f8a97
Róbert Göndöcs <gondocs.robert@gmail.com>
2025-11-29 16:13:03 +0100

    remove api_token column from users table

--- kts-monitor-app/routes/web.php
+++ kts-monitor-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SwaggerController;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,3 +18,8 @@ Route::get('/swagger.json', [SwaggerController::class, 'json']);
 Route::get('/test-route', function () {
     return 'web ok';
 });
+
+// Minimal login route for unauthenticated redirects (always 401 for this API-only app)
+Route::get('/login', function (Request $request) {
+    abort(401, 'Unauthenticated.');
+})->name('login');
